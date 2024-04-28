6887281d8c36fb54b4024059a7bee236054344c8
donutz03 <ionuthodoroaga@icloud.com>
2024-04-28 19:52:50 +0300

    Seminar 9 Arbori

---
+++ Task suplimentar 5 - Liste simplu inlantuite/Task suplimentar 5 - Liste simplu inlantuite.c
@@ -0,0 +1,14 @@
+/*
+Obiectele pe car ele cititi dintr-un fisier le salvati intr-o lista simplu inlantuita.
+
+1. Implementati o functie care sterge un nod de pe o pozitie data ca parametru. 
+Daca lista are mai putine noduri decat index-ul dat, nu se realizeaza stergerea.
+2. Implementati o functie care sa insereze elementele in cadrul listei simplu 
+inlantuite astfel incat acestea a fie sortate crescator dupa un camp la alegerea voastra.
+3. Implementati o functie care salveaza intr-un vector toate obiectele care indeplinesc o conditie stabilita de voi. 
+Realizati deep copy, astfel incat elementele din vector sa fie diferentiate de cele din lista. 
+Aveti grija la alocarea de spatiu, deoarece trebuie sa stabiliti pentru cate elemente veti aloca spatiu.
+4. Implementati o functie care primeste lista si doua pozitii. 
+In cadrul functiei trebuie sa interschimbati elementele de pe cele doua pozitii din lista primita.
+*/
+
